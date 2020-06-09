4b86a3ce5b86dc8af3a0e726d80723de7c2aac94
Your name <you@example.com>
2020-06-09 19:27:39 +0900

    commit

--- [4] Code 암기/Chapter 21. 제네릭(Generics) 1/GenericMethodTest.java
+++ [4] Code 암기/Chapter 21. 제네릭(Generics) 1/GenericMethodTest.java
@@ -1,21 +1,40 @@
-package _0603;
+package test0609;
 
-class Box<T> {
-	private T ob;
+/*
+//1. Box
+class Box {
+	private Object obj;
 
-	public void set(T o) {
-		ob = o;
+	// Getter method
+	public Object getObj() {
+		return obj;
+	}
+	// Setter method
+	public void setObj(Object obj) {
+		this.obj = obj;
 	}
 
+}
+*/
+
+//2. 1의 Object를 T로 다 바꿔 ~
+class Box<T> {
+	private T ob;
+	
+	// Getter method
 	public T get() {
 		return ob;
+	}
+	// Setter method
+	public void set(T ob) {
+		this.ob = ob;
 	}
 
 }
 
 class BoxFactory {
 
-	static <T> Box<T> makeBox(T o) {
+	public static <T> Box<T> makeBox(T o) {
 		Box<T> box = new Box<>();
 		box.set(o);
 		return box;
@@ -25,12 +44,14 @@ class BoxFactory {
 public class GenericMethodTest {
 
 	public static void main(String[] args) {
+
 		Box<String> sBox = BoxFactory.<String>makeBox("Sweet");
 		Box<Double> dBox = BoxFactory.<Double>makeBox(7.59);
-
 		System.out.println(sBox.get());
 		System.out.println(dBox.get());
 
 	}
 
 }
+
+
